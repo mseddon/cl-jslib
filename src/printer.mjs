9e3da3b4f1a1f196f8b8b49e8a70759328b8c497
mseddon <matt@scryetek.com>
2020-09-04 21:08:06 +0100

    Stub printer

---
+++ src/printer.mjs
@@ -0,0 +1,34 @@
+// copy-pprint-dispatch &optional (table *print-pprint-dispatch*)
+// pprint-dispatch object &optional (table *print-pprint-dispatch*)
+// pprint-fill stream object &optional (colon-p t) at-sign-p
+// pprint-linear stream object &optional (colon-p t) at-sign-p
+// pprint-tabular stream object &optional (colon-p t) at-sign-p (tabsize 16)
+// pprint-indent relative-to n &optional stream
+// pprint-newline kind &optional stream
+// pprint-tab kind column colinc &optional stream
+// print-object object stream
+// set-pprint-dispatch type-specifier function &optional (priority 0) (table *print-pprint-dispatch*)
+// write object &key (array *print-array*) (base *print-base*) (circle *print-circle*) (escape *print-escape*) (gensym *print-gensym*) (length *print-length*) (level *print-level*) (lines *print-lines*) (miser-width *print-miser-width*) (*print-pprint-dispatch* pprint-dispatch) (pretty *print-pretty*) (radix *print-radix*) (readably *print-readably*) (right-margin *print-right-margin*) (stream *standard-output*)
+// prin1 &optional output-stream
+// princ &optional output-stream
+// print &optional output-stream
+// pprint &optional output-stream
+// write-to-string write object &key (array *print-array*) (base *print-base*) (circle *print-circle*) (escape *print-escape*) (gensym *print-gensym*) (length *print-length*) (level *print-level*) (lines *print-lines*) (miser-width *print-miser-width*) (*print-pprint-dispatch* pprint-dispatch) (pretty *print-pretty*) (radix *print-radix*) (readably *print-readably*) (right-margin *print-right-margin*)
+// prin1-to-string object
+// princ-to-string object
+
+// *print-array*
+// *print-case*
+// *print-circle*
+// *print-escape*
+// *print-gensym*
+// *print-level*
+// *print-length*
+// *print-lines*
+// *print-miser-width*
+// *print-pprint-dispatch*
+// *print-pretty*
+// *print-readably*
+// *print-right-margin*
+// print-not-readable-object
+// format
