d5e267068ae00203199745eabb8951116b7e03d4
mseddon <matt@scryetek.com>
2020-09-07 11:40:34 +0100

    Begin pprint dispatch tables

--- src/main.mjs
+++ src/main.mjs
@@ -10,6 +10,7 @@ import * as symbols from "./symbols.mjs";
 import * as strings from "./strings.mjs";
 
 import { setInstance, LispInstance, lispInstance } from "./lisp-instance.mjs"
+import { PrettyPrintDispatchTable } from "./printer.mjs";
 
 function initializeInstance(inst) {
     setInstance(inst);
@@ -37,18 +38,18 @@ function initializeInstance(inst) {
     inst.PRINT_ARRAY = true;
     inst.PRINT_BASE  = 10;
     inst.PRINT_RADIX = false;
-    inst.PRINT_CASE = intern("UPCASE", inst.KEYWORD_PACKAGE);
+    inst.PRINT_CASE = symbols.intern("UPCASE", inst.KEYWORD_PACKAGE);
     inst.PRINT_CIRCLE = false;
     inst.PRINT_ESCAPE = true;
     inst.PRINT_GENSYM = true;
-    inst.PRINT_LEVEL  = NIL;
-    inst.PRINT_LENGTH = NIL;
-    inst.PRINT_LINES = NIL;
-    inst.PRINT_MISER_WIDTH = NIL;
-    inst.PRINT_PPRINT_DISPATCH = NIL;
+    inst.PRINT_LEVEL  = conses.NIL;
+    inst.PRINT_LENGTH = conses.NIL;
+    inst.PRINT_LINES = conses.NIL;
+    inst.PRINT_MISER_WIDTH = conses.NIL;
+    inst.PRINT_PPRINT_DISPATCH = new PrettyPrintDispatchTable();
     inst.PRINT_PRETTY = true;
     inst.PRINT_READABLY = false;
-    inst.PRINT_RIGHT_MARGIN = NIL;
+    inst.PRINT_RIGHT_MARGIN = conses.NIL;
 
     return inst;
 }
--- src/printer.mjs
+++ src/printer.mjs
@@ -1,16 +1,55 @@
 import { lispInstance } from "./lisp-instance.mjs";
-import { writeChar } from "./streams";
+import { writeChar } from "./streams.mjs";
+import { consp, car } from "./conses.mjs";
+import { eql } from "./equal.mjs"
+import { symbolp } from "./symbolp.mjs"
+
+export class PrettyPrintDispatchTable {
+    constructor(oldTable) {
+        if(oldTable) {
+            this.conses = new Map(oldTable.conses);
+            this.entries = [...oldTable.entries];
+        }
+    }
+
+    // Maps symbols onto printer functions when they are the car of a list.
+    //   (cons (member let flet)) for example would create an entry for LET and FLET in this map.
+    conses = new Map();
+
+    // More complex types that require more complex type discrimination.
+    entries = []
+}
 
 // copy-pprint-dispatch &optional (table *print-pprint-dispatch*)
+export function copyPPrintDispatch(table = lispInstance.PRINT_PPRINT_DISPATCH) {
+    return new PrettyPrintDispatchTable(table);
+}
+
+// set-pprint-dispatch type-specifier function &optional (priority 0) (table *print-pprint-dispatch*)
+
 // pprint-dispatch object &optional (table *print-pprint-dispatch*)
+export function pprintDispatch(object, table = lispInstance.PRINT_PPRINT_DISPATCH) {
+    if(consp(object) && symbolp(car(object))) {
+        let sym = car(object);
+        let dispatch = table.conses.get(sym);
+        if(dispatch) {
+            lispInstance.mv = [true];
+            return dispatch;
+        }
+    }
+    // okay, we want to just walk the entries then, picking the highest priority one that matches.
+
+    // return print-object
+}
+
 // pprint-fill stream object &optional (colon-p t) at-sign-p
 // pprint-linear stream object &optional (colon-p t) at-sign-p
 // pprint-tabular stream object &optional (colon-p t) at-sign-p (tabsize 16)
 // pprint-indent relative-to n &optional stream
 // pprint-newline kind &optional stream
 // pprint-tab kind column colinc &optional stream
+
 // print-object object stream
-// set-pprint-dispatch type-specifier function &optional (priority 0) (table *print-pprint-dispatch*)
 
 // write object &key (array *print-array*) (base *print-base*) (circle *print-circle*) (escape *print-escape*) (gensym *print-gensym*) (length *print-length*) (level *print-level*) (lines *print-lines*) (miser-width *print-miser-width*) (*print-pprint-dispatch* pprint-dispatch) (pretty *print-pretty*) (radix *print-radix*) (readably *print-readably*) (right-margin *print-right-margin*) (stream *standard-output*)
 export function write(object) {
